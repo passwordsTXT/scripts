a93abe1419ccb768e90e3a600b688c98c574e577
passwordsTXT <87912768+passwordsTXT@users.noreply.github.com>
2022-12-07 10:14:37 -0600

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "--------------------"
+echo "--------------------"
+echo "This is a test"
+echo "--------------------"
+echo "--------------------"
+
+touch test.txt
+echo "One Banana" >> test.txt
